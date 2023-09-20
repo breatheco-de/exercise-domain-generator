d2cfc94dc7e5ce063669b8509db7f0198787b171
Jose Mora <109150320+josemoracard@users.noreply.github.com>
2023-09-20 13:24:10 +0200

    Update app.js

--- app.js
+++ app.js
@@ -4,7 +4,10 @@
   let noun = ['jogger', 'racoon', 'dog', 'merchant', 'driver'];
   
   // generating the domains
-  for(let i = 0; i < pronoun.length; i++) 
-   for(let j = 0; j < adj.length; j++) 
-    for(let k = 0; k < noun.length; k++) 
-     console.log(pronoun[i] + adj[j] + noun[k]+'.com'); 
+  for(let i = 0; i < pronoun.length; i++) {
+    for(let j = 0; j < adj.length; j++) {
+      for(let k = 0; k < noun.length; k++) {
+        console.log(pronoun[i] + adj[j] + noun[k]+'.com'); 
+      }
+    }
+  }
